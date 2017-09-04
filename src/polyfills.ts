e4f0f4174fa6e0fed4b9c6aa00d223c34c980082
Daniel James Williams <v-wildan@microsoft.com>
2017-09-04 09:26:53 -0700

    Config dev to serve static content from /client.

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -1,4 +1,4 @@
-import '../assets/scripts/web-animations.min.js';
+import '../client/assets/scripts/web-animations.min.js';
 import 'core-js/es6';
 import 'core-js/es7/reflect';
 require('zone.js/dist/zone');
